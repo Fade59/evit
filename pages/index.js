48ab85e3deee94c451c4470919c4474c9bd9edfe
Fade59 <fd@itk.coach>
2022-12-30 10:55:13 +0100

    Inital commit

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,113 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+		<!-- site-header -->
+		<header id="kktfwp-masthead" class="kktfwp-header" role="banner">			
+			<div class="kktfwp-site-header-main kktfwp-row">
 
-      <Footer />
-    </div>
+	<div class="kktfwp-logo-menu-wrapper">
+
+	<div class="kktfwp-logo">
+		<a href="http://web.archive.org/web/20220107174004/https://evit.network/" id="logo--img" class="custom-logo-link" rel="home"><img class="site-logo" src="http://web.archive.org/web/20220107174004im_/https://evit.network/wp-content/uploads/2021/03/evit.network.png" alt="Essu" width="200" height="200"><img class="site-logo2x" src="http://web.archive.org/web/20220107174004im_/https://evit.network/wp-content/uploads/2021/03/evit.network.png" alt="Essu" width="200" height="200"></a>	</div>
+
+	<div id="kktfwp-site-header-menu" class="kktfwp-site-header-menu">
+					<nav class="kktfwp-main-navigation" role="navigation" aria-label="Primary Menu">
+				<ul id="menu-menu-1" class="kktfwp-primary-menu"><li id="menu-item-30" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-30"><a href="http://web.archive.org/web/20220107174004/https://evit.network/" aria-current="page">Impressum</a></li>
+<li id="menu-item-31" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><a href="http://web.archive.org/web/20220107174004/https://evit.network/datenschutzerklaerung/">Datenschutzerklärung</a></li>
+</ul>				
+				<div id="close-sb-menu">
+					<span></span>
+				</div>
+			</nav><!-- .main-navigation -->
+		
+	</div><!-- .site-header-menu -->	
+		
+	<div class="kktfwp-mobile">
+		<span class="kktfwp-ham ham-left"></span>
+		<span class="kktfwp-ham ham-right"></span>
+	</div>
+
+	</div>
+</div><!-- .site-header-main -->			
+		</header>
+		<!-- end site-header -->
+
+		<div class="kktfwp-site-content clearfix">
+<div class="kktfwp-content-area">
+
+	<main class="kktfwp-site-main" role="main">
+		
+					
+						
+			<!-- start page entry -->
+			<article id="post-6" class="post-6 page type-page status-publish hentry odd">
+			
+				<!-- start title -->
+				<div class="kktfwp-title clearfix">
+					<div class="kktfwp-title-wrapper">
+						<h1 class="entry-title"><span class="hide-title"><span>Impressum</span></span></h1>					</div>
+					<div class="kktfwp-description-wrapper">
+											</div>
+				</div>
+				<!-- end title -->
+
+				<div class="entry-content page-content">
+					
+<h2>Angaben gemäß § 5 TMG</h2>
+<p>evit eG<br>
+Gotenstr. 26<br>
+65205 Wiesbaden</p>
+
+<p>Genossenschaftsregister: GnR 423<br>
+Registergericht: Wiesbaden</p>
+
+<p><strong>Vertreten durch den Vorstand:</strong><br>
+Daniel Débs</p>
+
+<h2>Kontakt</h2>
+<p>Telefon: 06122/7783919<br>
+E-Mail: mail@evit.network</p>
+
+<h2>Umsatzsteuer-ID</h2>
+<p>Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br>
+DE341307633</p>
+
+<h2>Verbraucher­streit­beilegung/Universal­schlichtungs­stelle</h2>
+<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+<h3>Haftung für Inhalte</h3> <p>Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.</p> <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p> <h3>Haftung für Links</h3> <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p> <p>Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p> <h3>Urheberrecht</h3> <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.</p> <p>Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+
+
+<p></p>
+				</div><!-- .entry-content -->
+
+			</article>
+			<!-- end page entry -->
+
+			<!-- If comments are open or we have at least one comment, load up the comment template -->
+			
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+
+		</div><!-- .site-content -->
+
+		<footer class="kktfwp-footer" role="contentinfo">
+			<div class="kktfwp-row clearfix">
+
+				<div class="kktfwp-site-info">
+					evit eG 2021. Alle Rechte vorbehalten.				</div><!-- .site-info -->
+				
+				<div class="kktfwp-social">
+									</div><!-- .site-info -->
+				
+			</div>
+		</footer><!-- .site-footer -->
+		<div class="kktfwp-overlay"></div>
+	
   )
 }
